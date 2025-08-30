9ca3a6ea16aac443b6300b133891696501fbfec4
Sanchezz12 <juanjo.sanchez2013@gmail.com>
2025-08-30 13:15:44 -0500

    places

--- mi-carta/src/components/Mapa2.jsx
+++ mi-carta/src/components/Mapa2.jsx
@@ -11,12 +11,20 @@ function Mapa2() {
     }).addTo(map);
 
     const lugares = [
-      { nombre: "Pueblito Paisa", coords: [6.2359, -75.5751] },
-      { nombre: "Jardín Botánico", coords: [6.2705, -75.5657] },
-      { nombre: "Parque Arví", coords: [6.3106, -75.5036] },
-      { nombre: "El Tesoro", coords: [6.1928, -75.5463] },
-      { nombre: "Estadio Atanasio Girardot", coords: [6.2569, -75.5906] }
-    ];
+  { nombre: "Takamar Sushi - Mall San Lucas", coords: [6.2054, -75.5795] },
+  { nombre: "Gimnasio Smart Fit - La Intermedia", coords: [6.1675, -75.5919] },
+  { nombre: "Cinépolis City Plaza", coords: [6.1792, -75.5923] },
+  { nombre: "Centro Comercial Viva Envigado", coords: [6.1763, -75.5917] },
+  { nombre: "Calle de la Buena Mesa (Sushi World, Canibal, Sushi Roll Up, El Barral)", coords: [6.1764, -75.5918] },
+  { nombre: "Sushi Gama", coords: [6.2040, -75.5616] },
+  { nombre: "Sr. Buñuelo", coords: [6.2041, -75.5605] },
+  { nombre: "El Bosque Era Rosado", coords: [6.2045, -75.5612] },
+  { nombre: "Gitana en las Nubes", coords: [6.2043, -75.5614] },
+  { nombre: "Tierra Alta", coords: [6.2042, -75.5613] },
+  { nombre: "Biela Bakery", coords: [6.2044, -75.5611] },
+  { nombre: "El Diamante (Estadio Atanasio Girardot)", coords: [6.2569, -75.5906] }
+];
+
 
     lugares.forEach(lugar => {
       L.marker(lugar.coords).addTo(map).bindPopup(`<b>${lugar.nombre}</b>`);
