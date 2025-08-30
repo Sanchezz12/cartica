d98cc123167a78110564f94d505978950563c333
Sanchezz12 <juanjo.sanchez2013@gmail.com>
2025-08-30 11:11:23 -0500

    Build listo para GitHub Pages

--- mi-carta/vite.config.js
+++ mi-carta/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs',   // 👈 aquí le decimos que genere la carpeta docs
+  },
+  base: './',         // 👈 importante para GitHub Pages
 })
